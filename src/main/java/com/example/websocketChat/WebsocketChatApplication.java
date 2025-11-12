1eeadd4744eac2ed4048e25cba52d5e2d48a5365
hod284 <hod284@naver.com>
2025-11-13 00:05:48 +0900

    2123123323vghbb

--- src/main/java/com/example/websocketChat/WebsocketChatApplication.java
+++ src/main/java/com/example/websocketChat/WebsocketChatApplication.java
@@ -1,13 +1,19 @@
 package com.example.websocketChat;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.Console;
+
+@Slf4j
 @SpringBootApplication
 public class WebsocketChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketChatApplication.class, args);
+         log.info("WebsocketChatApplication started");
 	}
-
 }
