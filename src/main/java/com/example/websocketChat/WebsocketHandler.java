16e1c0c7cf6bb6d5ea21b1cfdce523a7c3190a9c
hod284 <dlfjs18@@>
2025-11-17 15:30:11 +0900

    절반 완성qwqw

--- src/main/java/com/example/websocketChat/WebsocketHandler.java
+++ src/main/java/com/example/websocketChat/WebsocketHandler.java
@@ -3,7 +3,6 @@ package com.example.websocketChat;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.validation.constraints.Null;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
